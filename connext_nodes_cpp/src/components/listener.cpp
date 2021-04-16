9e6c9fab90cf0e6507947d0b034c52e708296505
Andrea Sorbini <asorbini@rti.com>
2021-04-16 12:57:05 -0700

    Updated license

--- connext_nodes_cpp/src/components/listener.cpp
+++ connext_nodes_cpp/src/components/listener.cpp
@@ -11,6 +11,16 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// (c) 2021 Copyright, Real-Time Innovations, Inc.  All rights reserved.
+//
+// RTI grants Licensee a license to use, modify, compile, and create derivative
+// works of the Software.  Licensee has the right to distribute object form
+// only for use with RTI products.  The Software is provided "as is", with no
+// warranty of any type, including any warranty for fitness for any purpose.
+// RTI is under no obligation to maintain or support the Software.  RTI shall
+// not be liable for any incidental or consequential damages arising out of the
+// use or inability to use the software.
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
--- connext_nodes_cpp/src/standalone/listener_main.cpp
+++ connext_nodes_cpp/src/standalone/listener_main.cpp
@@ -1,4 +1,3 @@
-// Copyright 2021 Real-Time Innovations, Inc.
 // Copyright 2016 Open Source Robotics Foundation, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +11,16 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// (c) 2021 Copyright, Real-Time Innovations, Inc.  All rights reserved.
+//
+// RTI grants Licensee a license to use, modify, compile, and create derivative
+// works of the Software.  Licensee has the right to distribute object form
+// only for use with RTI products.  The Software is provided "as is", with no
+// warranty of any type, including any warranty for fitness for any purpose.
+// RTI is under no obligation to maintain or support the Software.  RTI shall
+// not be liable for any incidental or consequential damages arising out of the
+// use or inability to use the software.
 
 // Include RTI Connext DDS "modern C++" API
 #include <dds/dds.hpp>
